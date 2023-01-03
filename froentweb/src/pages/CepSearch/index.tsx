847689a7c378e3b1700acd3d6fa9a9076cce11c0
Erick Monteiro <monteiro.ck@hotmail.com>
2023-01-03 14:49:15 -0300

    finish form - integrating via api

--- froentweb/src/pages/CepSearch/index.tsx
+++ froentweb/src/pages/CepSearch/index.tsx
@@ -1,15 +1,46 @@
 import './styles.css';
 
 import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
+import axios from 'axios';
+
+//Representa os dados do formulario
+type FormData = {
+  cep: string;
+};
+
+type Address = {
+  logradouro: string;
+  localidade: string;
+};
 
 const CepSearch = () => {
+  const [address, setaddress] = useState<Address>();
+
+  const [formData, setFormData] = useState<FormData>({
+    cep: '',
+  });
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log('mudou para ' + event.target.value);
+    const name = event.target.name; //permite a mudança
+    const value = event.target.value;
+    setFormData({ ...formData, [name]: value }); //atualiza a mudança
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();//evita que o formulario seja enviado da forma padrão
-    console.log('Clicou no botão');
+    event.preventDefault(); //evita que o formulario seja enviado da forma padrão
+
+    //chama o axios
+    axios
+      .get(`https://viacep.com.br/ws/${formData.cep}/json`)
+      .then((response) => {
+        setaddress(response.data); //acessa os dados
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setaddress(undefined); //Não mostra o card dos valores do cep
+        console.log(error);
+      });
   };
 
   return (
@@ -20,6 +51,8 @@ const CepSearch = () => {
           <div className="form-container">
             <input
               type="text"
+              name="cep"
+              value={formData.cep}
               className="search-input"
               placeholder="CEP (somente números)"
               onChange={handleChange}
@@ -29,9 +62,12 @@ const CepSearch = () => {
             </button>
           </div>
         </form>
-
-        <ResultCard title="Logradouro" description="Lalala" />
-        <ResultCard title="Número" description="234" />
+        {address && ( //somente mostra o card caso o Address não esteja undefined
+          <>
+            <ResultCard title="Logradouro" description={address.logradouro} />
+            <ResultCard title="Localidade" description={address.localidade} />
+          </>
+        )}
       </div>
     </div>
   );
